91c89eec2ff87ecf98ec409bd1a6f97c3e9858fa
Daryl Sconyers <pilotnplayer@gmail.com>
2019-03-07 09:10:28 -0500

    Router change

--- src/app/features/scheduler/components/schedule-entry/schedule-entry.component.ts
+++ src/app/features/scheduler/components/schedule-entry/schedule-entry.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { PlayerAdded, SchedulerSettingsUpdated } from '../../actions/scheduler.actions';
 import { SchedulerState } from '../../reducers';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-schedule-entry',
@@ -14,7 +15,7 @@ export class ScheduleEntryComponent implements OnInit {
   seCourts = 0;
   sePlayersPerCourt = 0;
 
-  constructor(private store: Store<SchedulerState>) { }
+  constructor(private store: Store<SchedulerState>, private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,5 +25,6 @@ export class ScheduleEntryComponent implements OnInit {
       this.store.dispatch(new PlayerAdded(index, true, true, 0));
     }
     this.store.dispatch(new SchedulerSettingsUpdated(1, this.sePlayers, this.seCourts, this.sePlayersPerCourt));
+    this.router.navigate(['/schedule-generator']);
   }
 }
